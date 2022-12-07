3da8c89b220614e528757afb7aaa434430c296f6
ragaban <anton.parfilo@gmail.com>
2022-12-07 15:49:06 +0300

    comit

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<ContentComponent />} />
+          <Route path="/messenger" element={<ContentComponent />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
